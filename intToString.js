372daa48ef2a359fe896915a57a67160493e6385
JohnPena0115 <calmwinds06@gmail.com>
2021-12-20 14:48:17 -0500

    Read revision 2 if you like a good laugh.

--- intToString.js
+++ intToString.js
@@ -25,47 +25,67 @@ I will work on this in future revisions.
 
 */
 
+let int = 24 
 
+let string = ""
 
-function intToString(int) {
+string += int 
 
-    let reverseString = ""
-
-    if (int < 10) {
-
-        return reverseString += int
-
-    }
-
-    while (int / 10 > 0) {
-
-        reverseString += int % 10
-
-        int = Math.floor(int / 10)
-
-    }
+console.log(int)
+console.log(string)
 
 
 
+// function intToString(int) {
 
-    let stringIndexTracker = reverseString.length - 1
+//     let reverseString = ""
 
-    let numericString = ""
+    
 
-    while (stringIndexTracker > -1) {
+    // let negativeInt = false 
 
-        numericString += reverseString[stringIndexTracker]
+    // if (int < 0) {
 
-        stringIndexTracker--
-    }
+    //     negativeInt = true 
+    // }
 
-    return numericString
-}
+    // if (int < 10 && int > -10) {
 
-console.log(intToString(27))
-console.log(intToString(124567))
-console.log(intToString(6))
-console.log(intToString(0))
+    //     return reverseString += int
+
+    // }  
+
+    // while (int / 10 > 0) {
+
+    //     reverseString += int % 10
+
+    //     int = Math.floor(int / 10)
+
+    // }
+
+
+
+
+    // let stringIndexTracker = reverseString.length - 1
+
+    // let numericString = ""
+
+    // while (stringIndexTracker > -1) {
+
+    //     numericString += reverseString[stringIndexTracker]
+
+    //     stringIndexTracker--
+    // }
+
+    // return numericString
+// }
+
+// console.log(intToString(27))
+// console.log(intToString(124567))
+// console.log(intToString(6))
+// console.log(intToString(0))
+
+// console.log(typeof(intToString(-3)))
 
 /* 
 
@@ -100,6 +120,37 @@ Combined them together to make code more efficient
 
         return reverseString += int 
      }
+
+
+Revision 2 => 12-20-19 
+
+So it turns out the whole program was for naught. 
+
+When trying to make my program robust against negative int arguments  
+I accidentally discovered implicite string conversion. 
+
+I wrote the following code: 
+
+if (int < 0 && int > -10) {
+
+    reverseString += "-"
+    return reverseString += int 
+}
+
+Imagine my surprise when I console.logged intToString(-3) and received an output 
+of "--3"
+
+I had to laugh at myself realizing this program could have been written in 
+two lines. 
+
+It makes me wonder why bother having String() or toString() when you can 
+easily typecast a number using the "+=" operator. This program might never 
+have been a comedy of erros but it was definitely a comedy of assumptions. 
+
+For the sake of prosperity, a good laugh, and becuase I've already done most of the legwork, 
+I will suspend reality and assume "+=" implicit string conversion can only work 
+on a single numerical digit and not on a series of numerical digits/tokens. I will implement 
+the changes for this version of the program at a later date. 
 
 
 */
